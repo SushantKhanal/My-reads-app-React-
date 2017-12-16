c7a096bb193ba26f6c307a31da58cb6f969cc47b
Sushant Khanal <dreamerking114@gmail.com>
2017-12-16 17:47:42 +0545

    search bar is working

--- src/App.js
+++ src/App.js
@@ -3,25 +3,31 @@ import { Route } from 'react-router-dom'
 //import { Link } from 'react-router-dom'
 import HomePage from './HomePage'
 import SearchPage from './SearchPage'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class BooksApp extends React.Component {
   state = {
-    /**
-     * TODO: Instead of using this state variable to keep track of which page
-     * we're on, use the URL in the browser's address bar. This will ensure that
-     * users can use the browser's back and forward buttons to navigate between
-     * pages, as well as provide a good URL they can bookmark and share.
-     */
+    books: []
   }
+
+  componentDidMount() {  
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books }) 
+//same thing as above
+//      this.setState({ contacts: contacts })
+
+    } )
+  }  
 
   render() {
     return (
       <div className="app">
         <Route path='/search' render={() => (
           <div>
-            <SearchPage />
+            <SearchPage 
+            books={this.state.books}
+            />
           </div>
         )}/>
 
