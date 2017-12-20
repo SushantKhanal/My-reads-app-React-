82ee82263f57db1dfb9b145706eea8cd4bb004df
Sushant Khanal <dreamerking114@gmail.com>
2017-12-20 12:23:37 +0545

    HomePage component is made less Repetitive

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -14,10 +14,23 @@ class HomePage extends Component {
     this.setState({value: event.target.value})    
   }
 
+  bookShelves = [
+    {
+      shelfName:"currentlyReading",
+      shelfHeading: "Currently Reading"
+    }, 
+    {
+      shelfName:"wantToRead",
+      shelfHeading: "Want to Read"
+    }, 
+    {
+      shelfName:"read",
+      shelfHeading: "Read"
+    }         
+  ]
 
 
 	render() {
-    //console.log(this.state.read)
     return (
           <div className="list-books">
             <div className="list-books-title">
@@ -25,54 +38,17 @@ class HomePage extends Component {
             </div>
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                     {this.props.books.filter((book)=>
-                        (book.shelf==="currentlyReading")).map((book)=>(                  
-                          <Shelf key={book.id}
-                            book={book}
-                            handleChange={(book,e)=>{
-                              this.handleChange(book,e)
-                            }}                    
-                          />
-                      ))}                      
-                    </ol>
-                  </div>
-                </div> 
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                     {this.props.books.filter((book)=>
-                        (book.shelf==="wantToRead")).map((book)=>(                  
-                          <Shelf key={book.id}
-                            book={book}
-                            handleChange={(book,e)=>{
-                              this.handleChange(book,e)
-                            }}                    
-                          />
-                      ))}                      
-                    </ol>
-                  </div>
-                </div>     
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                     {this.props.books.filter((book)=>
-                        (book.shelf==="read")).map((book)=>(                  
-                          <Shelf key={book.id}
-                            book={book}
-                            handleChange={(book,e)=>{
-                              this.handleChange(book,e)
-                            }}                    
-                          />
-                      ))}                      
-                    </ol>
-                  </div>                          
-                </div>
+                {this.bookShelves.map((shelf)=>(
+                    <Shelf key={shelf.shelfName}
+                      books={this.props.books}
+                      heading={shelf.shelfHeading}
+                      name={shelf.shelfName}
+                      handleChange={(book,e)=>{
+                        this.handleChange(book,e)
+                      }}                    
+                    />
+                ))}              
+ 
               </div>
             </div>
             <div 
--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -3,8 +3,16 @@ import React, { Component } from 'react'
 class Shelf extends Component {
 
 	render() {
-    const { book } = this.props    
+    const { name } = this.props    
+    const { heading } = this.props    
+
 		return(
+      <div className="bookshelf" key={name}>
+        <h2 className="bookshelf-title">{heading}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+           {this.props.books.filter((book)=>
+              (book.shelf===name)).map((book)=>(        
                 <li key={book.id}>
                   <div className="book">
                     <div className="book-top">
@@ -22,7 +30,11 @@ class Shelf extends Component {
                     <div className="book-title">{book.title}</div>
                     <div className="book-authors">{book.authors}</div>
                   </div>
-                </li>                    
+                </li>       
+              ))}                      
+            </ol>
+          </div>
+        </div>                             
 		)
 	}
 }
