e2486373f1fb4103d91074b1bcd991d397653339
Sushant Khanal <dreamerking114@gmail.com>
2017-12-20 13:25:02 +0545

    functional stateless component to improve performance of application

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,24 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class Shelf extends Component {
-
-	render() {
-    const { name } = this.props    
-    const { heading } = this.props    
-
+//functional stateless component to improve performance of application 
+function Shelf(props) {
 		return(
-      <div className="bookshelf" key={name}>
-        <h2 className="bookshelf-title">{heading}</h2>
+      <div className="bookshelf" key={props.name}>
+        <h2 className="bookshelf-title">{props.heading}</h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-           {this.props.books.filter((book)=>
-              (book.shelf===name)).map((book)=>(        
+           {props.books.filter((book)=>
+              (book.shelf===props.name)).map((book)=>(        
                 <li key={book.id}>
                   <div className="book">
                     <div className="book-top">
                       <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
                       <div className="book-shelf-changer">
-                        <select value={book.shelf} onChange={(e) => this.props.handleChange(book,e)}>
+                        <select value={book.shelf} onChange={(e) => props.handleChange(book,e)}>
                           <option value="none" disabled>Move to...</option>
                           <option value="currentlyReading">Currently Reading</option>
                           <option value="wantToRead">Want to Read</option>
@@ -36,7 +32,6 @@ class Shelf extends Component {
           </div>
         </div>                             
 		)
-	}
 }
 
 export default Shelf
